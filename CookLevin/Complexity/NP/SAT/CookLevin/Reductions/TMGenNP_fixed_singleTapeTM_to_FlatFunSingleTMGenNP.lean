7bea96154ba45ec7f41ddc0da963bca392e4437a
Lean Researcher <lean-researcher@p-vs-np.github>
2026-05-06 15:10:58 +0000

    3> Verify lake build with C

--- CookLevin/Complexity/NP/SAT/CookLevin/Reductions/TMGenNP_fixed_singleTapeTM_to_FlatFunSingleTMGenNP.lean
+++ CookLevin/Complexity/NP/SAT/CookLevin/Reductions/TMGenNP_fixed_singleTapeTM_to_FlatFunSingleTMGenNP.lean
@@ -6,7 +6,7 @@ set_option autoImplicit false
 
 def TMGenNP_fixed_singleTapeTM_to_FlatFunSingleTMGenNP_instance {sig : finType}
     (inst : TMGenNPFixedInput sig) : flatTM × List Nat × Nat × Nat :=
-  ((), [], inst.maxSize, inst.steps)
+  (validFlatTM_default, [], inst.maxSize, inst.steps)
 
 theorem TMGenNP_fixed_singleTapeTM_to_FlatFunSingleTMGenNP {sig : finType} (M : TM sig 1) :
     TMGenNP_fixed M ⪯p FlatFunSingleTMGenNP := by
